f8561b9b377f0b64cf53c6d7b12a47659f28a72a
elarity <42677833@qq.com>
2020-11-26 10:00:29 +0800

    commit for ready

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"golang.org/x/sys/unix"
+	"log"
 )
 
 func main() {
@@ -12,5 +13,36 @@ func main() {
 	if err != nil {
 		fmt.Print(err)
 	}
+
+	filePath := "./api.log"
+	watcher, err := unix.InotifyAddWatch(inotifyFd, filePath, unix.IN_ALL_EVENTS)
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(watcher, " | err=", err)
+
+	/*
+		type InotifyEvent struct {
+		    Wd     int32
+		    Mask   uint32
+		    Cookie uint32
+		    Len    uint32
+		    Name   [0]uint8
+		}
+	*/
+	go func() {
+		var eventBuf [4096 * unix.SizeofInotifyEvent]byte
+		for {
+			readLength, err := unix.Read(inotifyFd, eventBuf[:])
+			if err != nil {
+				log.Fatal(err)
+				continue
+			}
+			var offset uint32
+			for offset <= uint32(readLength-unix.SizeofInotifyEvent) {
+
+			}
+		}
+	}()
 
 }
