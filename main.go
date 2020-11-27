ef9196fc1c3a1fe7d69963c1f78731e693cfcc71
elarity <42677833@qq.com>
2020-11-27 10:13:31 +0800

    commit for ready

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"golang.org/x/sys/unix"
 	"log"
-	"time"
 	"unsafe"
 )
 
 func main() {
 
 	inotifyFd, err := unix.InotifyInit()
-	fmt.Println(inotifyFd)
 	if err != nil {
 		fmt.Print(err)
 		log.Fatal(err)
@@ -23,18 +21,24 @@ func main() {
 		fmt.Print(err)
 		log.Fatal(err)
 	}
-	fmt.Println(watcher, " | err=", err)
 
-	/*
-		type InotifyEvent struct {
-		    Wd     int32
-		    Mask   uint32
-		    Cookie uint32
-		    Len    uint32
-		    Name   [0]uint8
-		}
-	*/
+	defer func() {
+		unix.Close(inotifyFd)
+		unix.Close(watcher)
+	}()
+
+	eventChannel := make(chan uint32)
+
 	go func() {
+		/*
+			type InotifyEvent struct {
+			    Wd     int32
+			    Mask   uint32
+			    Cookie uint32
+			    Len    uint32
+			    Name   [0]uint8
+			}
+		*/
 		var bufferIndex uint32
 		var readEventBufferLength uint32
 		// 每个SizeofInotifyEvent的大小是0x10，也就是16字节，但是还需要加上name长度
@@ -53,11 +57,13 @@ func main() {
 			for bufferIndex < readEventBufferLength {
 				// 从eventBuffer中开始按照顺序拿取第一个event
 				singleEvent := (*unix.InotifyEvent)(unsafe.Pointer(&eventBuffer[bufferIndex]))
+				eventChannel <- singleEvent.Mask
 				// 不知道为什么在监控目录时候，InotifyEvent的Name总是为空，连Go语法都过不了
-				// 所以只能通过这种方式委婉地获取文件名
+				// 所以只能通过这种方式委婉地获取文件名.
 				if singleEvent.Len > 0 {
 					fileNameByte := (*[unix.PathMax]byte)(unsafe.Pointer(&eventBuffer[bufferIndex+unix.SizeofInotifyEvent]))[:int(singleEvent.Len):int(singleEvent.Len)]
-					fmt.Println(string(fileNameByte))
+					fileName := string(fileNameByte)
+					fmt.Println(fileName)
 				}
 				bufferIndex = uint32(bufferIndex) + uint32(unix.SizeofInotifyEvent) + singleEvent.Len
 			}
@@ -65,6 +71,12 @@ func main() {
 	}()
 
 	for {
-		time.Sleep(10 * time.Second)
+		//time.Sleep(10 * time.Second)
+		select {
+		case event := <-eventChannel:
+			if event&unix.IN_CLOSE_WRITE == unix.IN_CLOSE_WRITE {
+				fmt.Println("lalala")
+			}
+		}
 	}
 }
